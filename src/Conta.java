7c1cf5a39aacf57768ac8fad9529a2a8b3c2936c
Marcos Felipe <101227242+marcosf01@users.noreply.github.com>
2023-01-24 12:12:49 -0300

    atualizacoes na regra de negocio

--- src/Conta.java
+++ src/Conta.java
@@ -1,18 +1,15 @@
 public class Conta {
     private double saldo;
     private int agencia;
-    private int numeroConta;
+    private int numero;
     private Cliente titular;
-
 
     public double getSaldo() {
         return this.saldo;
     }
-
     public void deposita(double valor) {
         this.saldo += valor;
     }
-
     public boolean saca(double valor) {
         if (this.saldo >= valor) {
             this.saldo -= valor;
@@ -20,23 +17,18 @@ public class Conta {
         }
         return false;
     }
-
     public void setAgencia(int agencia) {
         this.agencia = agencia;
     }
-
     public int getAgencia() {
         return this.agencia;
     }
-
-    public void setNumeroConta(int numero) {
-        this.numeroConta = numero;
+    public void setNumero(int numero) {
+        this.numero = numero;
     }
-
-    public int getNumeroConta() {
-        return this.numeroConta;
+    public int getNumero() {
+        return this.numero;
     }
-
     public boolean transfere(double valor, Conta destino) {
         if (saca(valor)) {
             destino.deposita(valor);
@@ -44,11 +36,9 @@ public class Conta {
         }
         return false;
     }
-
     public void setTitular(Cliente titular) {
         this.titular = titular;
     }
-
     public Cliente getTitular() {
         return this.titular;
     }
